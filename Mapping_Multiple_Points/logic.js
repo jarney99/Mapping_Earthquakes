ba761d02583e963b70378be0cc4d883f4073a5dd
jarney99 <95194554+jarney99@users.noreply.github.com>
2022-03-23 13:57:54 -0500

    updates

--- Mapping_Multiple_Points/logic.js
+++ Mapping_Multiple_Points/logic.js
@@ -3,6 +3,22 @@ console.log("working");
 // We create the map object with options.
 let map = L.map('mapid').setView([34.0522, -118.2437], 14);
 
+// Coordinates for each point to be used in the polyline.
+let line = [
+  [33.9416, -118.4085],
+  [37.6213, -122.3790],
+  [40.7899, -111.9791],
+  [47.4502, -122.3088]
+];
+
+
+// Create a polyline using the line coordinates and make the line yellow.
+L.polyline(line, {
+  color: "yellow"
+}).addTo(map);
+
+
+
 // Get data from cities.js
 let cityData = cities;
 
